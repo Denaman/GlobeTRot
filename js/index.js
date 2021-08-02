2f472a0c8044559f8264f91e594589a514cb3223
Denaman <amanfohdennis@yahoo.com>
2021-08-02 07:02:35 +0000

    Implement the initMap

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
         //Function search() that will be used in the input element
-        function search(){
+       /* function search(){
             //An if statement that checks what key we are using (ascii code for enter key 13)
             if(event.keyCode == 13){
                 //Query document with the getElementById attribute to target the input element, getting the value from the user
@@ -10,5 +10,13 @@
                 //Added a zoom parameter in the API call for when a user search a location (&zoom=15).
                 document.getElementById('map').setAttribute('src', `https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&zoom=15&q=${location}`)
             }
-        }
+        }*/
+        //The initMap
+        function initMap() {
+            const sydney = new google.maps.LatLng(-33.867, 151.195);
+            infoWindow = new google.maps.InfoWindow();
+            map = new google.maps.Map(document.getElementById("map"), {
+                center: sydney,
+                zoom: 14,
+            });
     
