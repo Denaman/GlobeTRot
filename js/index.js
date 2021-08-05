4a93ec7b6ba0246eec52823812a2815ae1c237ab
Denaman <amanfohdennis@yahoo.com>
2021-08-05 21:45:07 +0000

    Implement the request making function and the create marker

--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
 
-        //Function search() that will be used in the input element
-       /* function search(){
-            //An if statement that checks what key we are using (ascii code for enter key 13)
-            if(event.keyCode == 13){
-                //Query document with the getElementById attribute to target the input element, getting the value from the user
-                let location = document.getElementById("test").value;
-                console.log(location)
-                //Query the document with the getElementById attribute to target the iframe element. Using the setAttribute method to change the src.
-                //Added a zoom parameter in the API call for when a user search a location (&zoom=15).
-                document.getElementById('map').setAttribute('src', `https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&zoom=15&q=${location}`)
+      let map;
+      let service;
+      let infoWindow;
+
+      //Request making function and the create marker
+      function makeRequest(keywordText){
+        const request = {
+            query: keywordText,
+            fields: ["geometry"],
+        };
+    
+        service = new google.maps.places.PlacesService(map);
+        service.findPlaceFromQuery(request, (results, status) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+                // The create marker
+                for (let i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+                map.setCenter(results[0].geometry.location);
             }
-        }*/
+        });
+    }
         //The initMap
         function initMap() {
             const sydney = new google.maps.LatLng(-33.867, 151.195);
